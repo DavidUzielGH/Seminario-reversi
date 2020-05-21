3fdbc7754061fd626924b124a2cae21db90498ca
David Uziel Guevara Hernández <daviduziel.gh@gmail.com>
2020-05-21 18:32:23 -0500

    Se puede capturar en vertical y horizontal

--- PROYECTO.c
+++ PROYECTO.c
@@ -8,8 +8,11 @@
 #include <stdlib.h>
 #define TURNO_O 0
 #define TURNO_X 1
+#define true 1
+#define false 0
 
 struct Tablero{
+  int turno;
   char tabla[8][8];
   char fichasO[64][2];
   char fichasX[64][2];
@@ -31,11 +34,10 @@ void dibujarTablero(char tablero[8][8], int cursor[2]){ //aqui se dibuja el tabl
         printf("╚════════╝");
       }
       if(i > 0 && j > 0 && i < 9 && j < 9){
-	if (i-1 == cursor[0] && j-1 == cursor[1]) {
+	if (j-1 == cursor[0] && i-1 == cursor[1]) {
 	  printf("#");
 	} else {
-	  
-	printf("%c", tablero[i-1][j-1]);
+	printf("%c", tablero[j-1][i-1]);
 	}
       }
     }
@@ -43,8 +45,124 @@ void dibujarTablero(char tablero[8][8], int cursor[2]){ //aqui se dibuja el tabl
   }
 }
 
+int busquedaX(struct Tablero tablero, int direccion){
+  int movimientoX = 0;
+  char YO, ENEMIGO; //FICHAS DEL ENEMIGO Y PROPIAS, RESPECTIVAMENTE
+  int cursor;
+  if (tablero.turno == TURNO_O){
+    YO = 'O';
+    ENEMIGO = 'X';
+  }
+  if (tablero.turno == TURNO_X){
+    YO = 'X';
+    ENEMIGO = 'O';
+  }
+  for (int i = tablero.cursor[0]+direccion; i <= 8 && i >= -1; i+=direccion) {
+      if (i == 8 || i == -1) {
+	movimientoX = 0;
+	break;
+      }
+    if (tablero.tabla[i][tablero.cursor[1]] == ENEMIGO){
+      movimientoX++;
+    } else{
+      if (tablero.tabla[i][tablero.cursor[1]] == YO) {
+	break;
+      } else if(tablero.tabla[i][tablero.cursor[1]] == ' '){
+	movimientoX = 0;
+	break;
+      } else{
+	movimientoX = 0;
+	break;
+      }
+    }
+  }
+  return movimientoX;
+}
+
+int busquedaY(struct Tablero tablero, int direccion){
+  int movimientoY = 0;
+  char YO, ENEMIGO; //FICHAS DEL ENEMIGO Y PROPIAS, RESPECTIVAMENTE
+  if (tablero.turno == TURNO_O){
+    YO = 'O';
+    ENEMIGO = 'X';
+  }
+  if (tablero.turno == TURNO_X){
+    YO = 'X';
+    ENEMIGO = 'O';
+  }
+  for (int i = tablero.cursor[1]+direccion;i <= 8 && i >= -1; i+=direccion) {
+    if (tablero.tabla[tablero.cursor[0]][i] == ENEMIGO){
+      movimientoY++;
+      if (i == 8 || i == -1) {
+	movimientoY = 0;
+	break;
+      }
+    } else {
+      if (tablero.tabla[tablero.cursor[0]][i] == YO) {
+	break;
+      } else if(tablero.tabla[tablero.cursor[0]][i] == ' '){
+	movimientoY = 0;
+	break;
+      } else{
+	movimientoY = 0;
+	break;
+      }
+    }
+  }
+  return movimientoY;
+}
+int toggleTurno(int turnoActual){
+  if (turnoActual == TURNO_O) return TURNO_X;
+  else return TURNO_O;
+}
+
+struct Tablero buscarReemplazar(struct Tablero tablero){
+  const int ADELANTE = 1;
+  const int ATRAS = -1;
+  char YO;
+  if (tablero.turno == TURNO_O) YO = 'O';
+  if (tablero.turno == TURNO_X) YO = 'X';
+
+  int flagMovimientoValido = false;
+  int cursorX = tablero.cursor[0];
+  int cursorY = tablero.cursor[1]; 
+  int incrementoX = busquedaX(tablero, ADELANTE);
+  int decrementoX = busquedaX(tablero, ATRAS);
+  int incrementoY = busquedaY(tablero, ADELANTE);
+  int decrementoY = busquedaY(tablero, ATRAS);
+  if (incrementoX > 0) {
+    for (int i = cursorX; i <= cursorX+incrementoX; ++i) {
+      tablero.tabla[i][cursorY] = YO;
+    }
+    flagMovimientoValido = true;
+  }
+  if (decrementoX > 0) {
+    for (int i = cursorX; i >= cursorX-decrementoX; --i) {
+      tablero.tabla[i][cursorY] = YO;
+    }
+    flagMovimientoValido = true;
+  }
+  if (incrementoY > 0) {
+    for (int i = cursorY; i <= cursorY+incrementoY; ++i) {
+      tablero.tabla[cursorX][i] = YO;
+    }
+    flagMovimientoValido = true;
+  }
+  if (decrementoY > 0) {
+    for (int i = cursorY; i >= cursorY-decrementoY; --i) {
+      tablero.tabla[cursorX][i] = YO;
+    }
+    flagMovimientoValido = true;
+  }
+  if(flagMovimientoValido == true) tablero.turno = toggleTurno(tablero.turno);
+  return tablero;
+}
+
 struct Tablero realizarJugada(struct Tablero tablero){
-  int cursor[2] = *tablero.cursor;
+  // "Si estas encima de una ficha, no hagas nada"
+  if (tablero.tabla[tablero.cursor[0]][tablero.cursor[1]] == 'X')  return tablero; 
+  if (tablero.tabla[tablero.cursor[0]][tablero.cursor[1]] == 'O')  return tablero;
+  tablero = buscarReemplazar(tablero);
   return tablero;
 }
 
@@ -53,7 +171,7 @@ int main(){
   int indiceO = 0;
   int fin = 0;
   char opc; 
-  int turno = TURNO_O;
+  tablero.turno = TURNO_O;
   tablero.cursor[0] = 0;
   tablero.cursor[1] = 0;
   for (int i = 0; i < 9; ++i) { // se llena el tablero de espacios en blanco
@@ -68,7 +186,7 @@ int main(){
   tablero.tabla[4][3] = 'O';
   while (opc != 'q') {
     system("cls");
-    if (turno == TURNO_O) { // Aqui solo se imprime de quien es el turno
+    if (tablero.turno == TURNO_O) { // Aqui solo se imprime de quien es el turno
       printf("TURNO DE O\n");
     } else {
       printf("TURNO DE X\n");
@@ -77,24 +195,24 @@ int main(){
     printf("mover:wasd, seleccionar:e, salir:q\n");
     scanf(" %c", &opc);
     switch(opc){ //Este switch controla el movimiento del cursor
-      case 's':
-	if (tablero.cursor[0] < 7) {
-	  tablero.cursor[0]++;
-	}
-	break;
-      case 'w':
+      case 'a':
 	if (tablero.cursor[0] > 0) {
 	  tablero.cursor[0]--;
 	}
 	break;
-      case 'a':
-	if (tablero.cursor[1] > 0) {
-	  tablero.cursor[1]--;
+      case 'd':
+	if (tablero.cursor[0] < 7) {
+	  tablero.cursor[0]++;
 	}
 	break;
-      case 'd':
+      case 's':
 	if (tablero.cursor[1] < 7) {
 	  tablero.cursor[1]++;
+	}
+	break;
+      case 'w':
+	if (tablero.cursor[1] > 0) {
+	  tablero.cursor[1]--;
 	}
 	break;
       case 'e':
