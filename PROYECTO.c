f7f2074634f2aff3cc37080a4ee6297ba5c1873b
David Uziel Guevara Hernández <daviduziel.gh@gmail.com>
2020-05-21 10:16:28 -0500

    Cambie los datos del tablero en un struct

--- PROYECTO.c
+++ PROYECTO.c
@@ -9,6 +9,13 @@
 #define TURNO_O 0
 #define TURNO_X 1
 
+struct Tablero{
+  char tabla[8][8];
+  char fichasO[64][2];
+  char fichasX[64][2];
+  int cursor[2];
+}tablero; 
+
 void dibujarTablero(char tablero[8][8], int cursor[2]){ //aqui se dibuja el tablero gg
   for (int i = 0; i < 10; ++i) {
     for (int j = 0; j < 10; ++j) {
@@ -36,26 +43,29 @@ void dibujarTablero(char tablero[8][8], int cursor[2]){ //aqui se dibuja el tabl
   }
 }
 
+struct Tablero realizarJugada(struct Tablero tablero){
+  int cursor[2] = *tablero.cursor;
+  return tablero;
+}
+
 int main(){
   setlocale(LC_ALL, "es_ES");
   int indiceO = 0;
   int fin = 0;
-  char tablero[8][8];
-  char fichasO[64][2];
-  char fichasX[64][2];
-  int cursor[2] = {0, 0};
   char opc; 
   int turno = TURNO_O;
+  tablero.cursor[0] = 0;
+  tablero.cursor[1] = 0;
   for (int i = 0; i < 9; ++i) { // se llena el tablero de espacios en blanco
     for (int j = 0; j < 9; ++j) {
-      tablero[i][j] = ' ';
+      tablero.tabla[i][j] = ' ';
     }
   }
   char especial;
-  tablero[3][3] = 'X'; //Se pone en el tablero las piezas iniciales
-  tablero[4][4] = 'X';
-  tablero[3][4] = 'O';
-  tablero[4][3] = 'O';
+  tablero.tabla[3][3] = 'X'; //Se pone en el tablero las piezas iniciales
+  tablero.tabla[4][4] = 'X';
+  tablero.tabla[3][4] = 'O';
+  tablero.tabla[4][3] = 'O';
   while (opc != 'q') {
     system("cls");
     if (turno == TURNO_O) { // Aqui solo se imprime de quien es el turno
@@ -63,30 +73,32 @@ int main(){
     } else {
       printf("TURNO DE X\n");
     }
-    dibujarTablero(tablero, cursor);
+    dibujarTablero(tablero.tabla, tablero.cursor);
     printf("mover:wasd, seleccionar:e, salir:q\n");
     scanf(" %c", &opc);
     switch(opc){ //Este switch controla el movimiento del cursor
       case 's':
-	if (cursor[0] < 7) {
-	  cursor[0]++;
+	if (tablero.cursor[0] < 7) {
+	  tablero.cursor[0]++;
 	}
 	break;
       case 'w':
-	if (cursor[0] > 0) {
-	  cursor[0]--;
+	if (tablero.cursor[0] > 0) {
+	  tablero.cursor[0]--;
 	}
 	break;
       case 'a':
-	if (cursor[1] > 0) {
-	  cursor[1]--;
+	if (tablero.cursor[1] > 0) {
+	  tablero.cursor[1]--;
 	}
 	break;
       case 'd':
-	if (cursor[1] < 7) {
-	  cursor[1]++;
+	if (tablero.cursor[1] < 7) {
+	  tablero.cursor[1]++;
 	}
 	break;
+      case 'e':
+	tablero = realizarJugada(tablero);
       case 'q':
 	break;
       default:break;
